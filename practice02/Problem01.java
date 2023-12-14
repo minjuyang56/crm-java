7f13242b506d9426bfa1a19b9ca68361aecdda27
minjuyang56 <alswn0558@gmail.com>
2023-12-14 12:35:42 +0900

    practice02 모두 구현 완료

--- practice02/Problem01.java
+++ practice02/Problem01.java
@@ -10,17 +10,16 @@ public class Problem01 {
         int sum = 0;
 
         //배열의 처음부터 끝까지 순회
-        for () {
-            
+        for (int i=0; i<data.length; i++) {
             //현재 배열의 값이 3의 배수인지 검사
-            if () {
+            if (data[i] % 3 == 0) {
                 count++;
                 sum += data[i];
             }
 
         }
-        System.out.println("주어진 배열에서 3의 배수의 개수=>" + count);
-        System.out.println("주어진 배열에서 3의 배수의 합=>" + sum);
+        System.out.println("주어진 배열에서 3의 배수의 개수=> " + count);
+        System.out.println("주어진 배열에서 3의 배수의 합=> " + sum);
     }
 
 }
--- practice02/Problem02.java
+++ practice02/Problem02.java
@@ -9,21 +9,22 @@ public class Problem02 {
         Scanner sc = new Scanner(System.in);
         int[] dataArray = new int[5];
         int num;
-        double sum =0;
+        double sum = 0;
         
         //키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드
         for(int i=0; i<dataArray.length; i++){
             num = sc.nextInt();
             //입력받은값 현재의 배열위치에저장
+            dataArray[i] = num;
         }
         
         //배열에 저장된 정수 값 더하기
-        for(){
-           
+        for(int i=0; i<dataArray.length; i++){
+            sum += dataArray[i];
         }
         
         //평균값 출력
-        System.out.println();
+        System.out.println("평균은 " + sum/dataArray.length + " 입니다.");
         
         sc.close();
     }
