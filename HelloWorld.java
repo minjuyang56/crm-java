109acf08cbad664e5687da149756f8cc3a5efa08
minjuyang56 <alswn0558@gmail.com>
2023-12-14 12:43:32 +0900

    코드 정리

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,10 @@ public class HelloWorld{
     public static void main(String[] args){
         System.out.println("입력하세욤");
         Scanner s = new Scanner(System.in);
-        
-        int month = s.nextInt(); 
+    
         int day_per_month = 0;
 
         System.out.println(day_per_month + "일");
+        s.close();
     }
 }
--- practice01/Problem06.java
+++ practice01/Problem06.java
@@ -7,11 +7,15 @@ public class Problem06 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("숫자를 입력하세요");
+
 		int num = sc.nextInt();
 		int i;
 		int sum = 0 ;
 		
+		sc.close();
+
 		if (num % 2 == 0){
 			for(i=2; i<=num; i+=2){
 				sum += i;
@@ -21,8 +25,8 @@ public class Problem06 {
 				sum += i;
 			}
 		}
+
 		
 		System.out.println("결과값: " + sum);
-		sc.close();
 	}
 }
